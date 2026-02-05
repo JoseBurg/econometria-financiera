9d262404bc23173c563c1085913c2732aeef7e7a
José Antonio Burgos Francisco <65372353+JoseBurg@users.noreply.github.com>
2026-02-05 16:52:49 -0400

    Refactor IPC analysis and update data path
    Updated the script to include new data path and modified ACF analysis for IPC time series. Added comments for clarity on unit root tests and data trends.

--- Practica1/ejercicios.R
+++ Practica1/ejercicios.R
@@ -4,10 +4,11 @@ library(tseries)
 library(urca)
 library(fpp3)
 
+#path_general <- "./1. Materias/econometria-financiera"
 
-#source("./Practica1/functions.R")
-
-path_ipc <- "ipc.xlsx"
+path_ipc <- file.path(
+ # path_general, 
+  "ipc.xlsx")
 
 
 ipc <- readxl::read_excel(path_ipc)
@@ -34,14 +35,84 @@ base_ipc_longer <- base_ipc |>
   )
 
 
-
-
-# Visualización de las Series -----------
-
-
 # Análisis de Raíz Unitaria -----------------------------------------------
 ipc_ts <- base_ipc |> 
-  select(fecha, matches("(log|dif1|vi)$")) |> 
+  select(fecha, contains("log")) |> 
   mutate(fecha = yearmonth(fecha)) |> 
   as_tsibble(index = fecha)
+
+
+ipc_ts |> 
+  ACF(ipc_transporte_log, lag_max = 36) |> # función de autocorrelación
+  autoplot() +
+  theme_classic()
+
+# ACF -------
+ipc_ts |> 
+  pivot_longer(
+    cols = -fecha, 
+    names_to = "ipc",
+    values_to = "valor"
+  ) |> 
+  mutate(ipc = stringr::str_remove(ipc, "_log")) |> 
+  group_by(ipc) |> 
+  ACF(valor, lag_max = 24) |> 
+  autoplot() +
+  theme_classic() + 
+  labs(title = "Logaritmos de IPC") +
+  theme_conf()
+
+# ACF -------
+ipc_ts |> 
+  pivot_longer(
+    cols = -fecha, 
+    names_to = "ipc",
+    values_to = "valor"
+  ) |> 
+  mutate(ipc = stringr::str_remove(ipc, "_log")) |> 
+  group_by(ipc) |> 
+  ACF(valor, lag_max = 24, type = "partial") |> 
+  autoplot() +
+  theme_classic() + 
+  labs(title = "Logaritmos de IPC") +
+  theme_conf()
+
+
+
+
+
+# Cuando los datos presentan una tendencia, las autocorrelaciones para pequeños rezagos tienden a ser grandes y positivas, ya que las observaciones cercanas en el tiempo también tienen valores cercanos. Por lo tanto, la ACF de una serie temporal con tendencia tiende a tener valores positivos que disminuyen lentamente a medida que aumentan los rezagos.  
+
+# Cuando los datos son estacionales, las autocorrelaciones serán mayores para los rezagos estacionales (en múltiplos del período estacional) que para otros rezagos.
+
+# Contraste de raíz unitaria ----------------------------------------------
+
+ipc_ts |> 
+  features(ipc_log, unitroot_kpss)
+
+# La hipotesis nula es que los datos son estacionario,
+# debido a que p-value es menor 0.05, se rechaza la 
+# hipotesis nula, lo datos no son estacionario.
+
+ipc_ts |> 
+  mutate(ipc_log = difference(ipc_log)) |> 
+  features(ipc_log, unitroot_kpss)
+
+ts_ipc <- ts(
+  ipc |> select(-fecha), 
+  start = c(2006, 01), 
+  frequency = 12) |> 
+  as.list()
+
+log_ts <- purrr::map(ts_ipc, log)         # Series en logaritmos
+
+test_log <- purrr::map(log_ts, ~urca::ur.df(.x, type = "trend", lags = 4))
+
+test_summary <- purrr::map(test_log, summary)
+
+test_summary$ipc@teststat[1,2]
+test_summary$ipc@cval[1,2]
+
+
+log_diff <- purrr::map(log_ts, diff)     # Primera diferencia de los logaritmos
 
